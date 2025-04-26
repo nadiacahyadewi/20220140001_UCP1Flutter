3f3318c354c78f7b8f085e34d1a00243e04a2be3
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-26 22:26:47 +0700

    membuat profil di HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1/login_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+        return Scaffold(
+      backgroundColor: Colors.white, 
+      body: Column(
+        children: [
+          Container(
+            color: Colors.green,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            child: SafeArea(
+              bottom: false,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+                crossAxisAlignment: CrossAxisAlignment.center, 
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.white,
+                        backgroundImage: AssetImage('assets/images/profil.png'),
+                      ),
+                      const SizedBox(width: 15),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Selamat Datang',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          Text(
+                            'Admin',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Konten lainnya di sini
+        ],
+      ),
+    );
   }
 }
