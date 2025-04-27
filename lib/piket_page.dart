cad5696f696cb2557d89631614b1765137f38f85
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-27 10:53:40 +0700

    membuat file halaman piket menggunakan statefulWidget

---
+++ lib/piket_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PiketPage extends StatefulWidget {
+  const PiketPage({super.key});
+
+  @override
+  State<PiketPage> createState() => _PiketPageState();
+}
+
+class _PiketPageState extends State<PiketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
