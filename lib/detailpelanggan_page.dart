66e4c2dd14b4199c358daa44a5af674d8af00038
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-27 20:27:10 +0700

    membuat halaman detailPelanggan menggunakan stateful widget

---
+++ lib/detailpelanggan_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailpelangganPage extends StatefulWidget {
+  const DetailpelangganPage({super.key});
+
+  @override
+  State<DetailpelangganPage> createState() => _DetailpelangganPageState();
+}
+
+class _DetailpelangganPageState extends State<DetailpelangganPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
