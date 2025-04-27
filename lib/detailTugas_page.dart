d40dd9430a3bb98655d66953023ac4c22298549f
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-27 16:01:42 +0700

    membuat halaman detailtugas yang tersambung oleh halaman piket

--- lib/detailTugas_page.dart
+++ lib/detailTugas_page.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
 
-class DetailtugasPage extends StatelessWidget {
-  const DetailtugasPage({super.key});
+class DetailTugasPage extends StatelessWidget {
+  final String tugas;
+  final String tanggal;
+  final String nama;
+
+  const DetailTugasPage({
+    super.key, 
+    required this.tugas, 
+    required this.tanggal, 
+    required this.nama
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          'Detail $tugas',
+          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+        ),
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  tanggal,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+        
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text(
+                    nama,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            
+            const SizedBox(height: 30),
+            
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(
+                tugas,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
