0280bd08bfb6c3b0fa250f6e2a3ce02c435e8b40
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-27 21:15:00 +0700

    membuat file halaman pendataan barang menggunakan stateful widget

---
+++ lib/barang_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BarangPage extends StatefulWidget {
+  const BarangPage({super.key});
+
+  @override
+  State<BarangPage> createState() => _BarangPageState();
+}
+
+class _BarangPageState extends State<BarangPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
